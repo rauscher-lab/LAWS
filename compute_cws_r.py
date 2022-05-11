fd90e67e885e1835db98464aaaa273d26364b76f
Eugene Klyshko <klyshko.jack@gmail.com>
2022-05-11 10:45:21 -0400

    Update compute_cws_r.py

--- compute_cws_r.py
+++ compute_cws_r.py
@@ -4,16 +4,33 @@
 #  References in the code are related to the main text of the manuscript
 # 
 #  Developer: Eugene Klyshko 
+# This code computes offset vectors from the nearest water molecules to each crystallographic water sites
+# in each frame of the MD simulation
 
+## (!!!) User should adjust these parameters according to their system and file names:
 
-## This code computes offset vectors from the nearest water molecules to each crystallographic water sites
-## in each frame of the MD simulation
+# 1. Path to a simulation structure and trajectory (which includes positions of all water molecules)
+traj_folder = './'
+struct = traj_folder + 'firstframe.gro'
+trajectory = traj_folder + 'trajectory.xtc'
 
-# These parameters need to be adjusted by the user:
+# 2. Path to the structure of the system (single protein, unit cell or supercell) containing only protein and CWS. 
+# Typically, a crystal structure (PDB or GRO file) from which your simulation system was built.
+# In case of the MD simulation of a protein crystal (as in the manuscript), the unit cell structure was constructed using option `build a unit cell` in CHARMM-GUI server,
+# by also preserving all crystallographic water oxygens. 
+# Note: Protein atoms numbering must be the same as in your trajectory file. It is normally the case since the MD system is constructed consequtively: 
+# PDB (protein + CWS coordinates) -> solvating the system with H2O (using for example, gmx solvate) -> adding ions.
+# Initial information for multilateration (coordinating protein atoms for each CWS) will be extracted from this structure file. 
+CRYSTAL_STRUCTURE = 'crystal.gro' # Protein atom numbers in this file should correpond to 'firstframe.gro'
 
+# 3. Parameters of the system and trajectory
+stride = 10 # Stride for analysis (when stride=10 we will analyze only every 10-th frame of the original trajectory)
+N_chains = 4 # Number of symmetric chains in the simulation. In the manuscript, we have a unit cell with 4 protein chains. 
+N_atoms_in_chain = 1473 # Number of protein atoms in each chain.
+n_waters = 94 # Number of CWS in the crystal structure.
 
-
-
+# 4. Path to a folder where visualized CWS positions (as a new trajectory) will be saved. Can be left unchanged.
+folder_to_save = 'visualize/'
 
 
 # import useful libraries, such as MDAnalysis, and necessary functions
@@ -45,48 +62,21 @@ from laws import (
 # Using a parallel (MPI) to speed up the computations across the timeframes
 from mpi4py import MPI
 
-## (!!!) User should adjust these parameters according to their system and file names:
-
-# 1. Path to a simulation structure and trajectory (which includes positions of all water molecules)
-traj_folder = './'
-struct = traj_folder + 'firstframe.gro'
-trajectory = traj_folder + 'trajectory.xtc'
-
-# 2. Path to the structure of the system (single protein, unit cell or supercell) containing only protein and CWS. 
-# Typically, a crystal structure (PDB or GRO file) from which your simulation system was built.
-# In case of the MD simulation of a protein crystal (as in the manuscript), the unit cell structure was constructed using option `build a unit cell` in CHARMM-GUI server,
-# by also preserving all crystallographic water oxygens. 
-# Note: Protein atoms numbering must be the same as in your trajectory file. It is normally the case since the MD system is constructed consequtively: 
-# PDB (protein + CWS coordinates) -> solvating the system with H2O (using for example, gmx solvate) -> adding ions.
-# Initial information for multilateration (coordinating protein atoms for each CWS) will be extracted from this structure file. 
-CRYSTAL_STRUCTURE = 'crystal.gro' # Protein atom numbers in this file should correpond to 'firstframe.gro'
-
-# 3. Parameters of the system and trajectory
-stride = 10 # Stride for analysis (when stride=10 we will analyze only every 10-th frame of the original trajectory)
-N_chains = 4 # Number of symmetric chains in the simulation. In the manuscript, we have a unit cell with 4 protein chains. 
-N_atoms_in_chain = 1473 # Number of protein atoms in each chain.
-n_waters = 94 # Number of CWS in the crystal structure.
-
-# 4. Path to a folder where visualized CWS positions (as a new trajectory) will be saved. Can be left unchanged.
-folder_to_save = 'visualize/'
-
 # This function assumes that atomic coordinates are written consequtively (chain by chain) in the structure file from the very beginning of the file.
 # It creates MDAnalysis selection for each chain (can be applied to both CRYSTAL_STRUCTURE and MD struct) for further analysis
 if N_chains >= 1:
-    chains = find_chains()
+    chains = find_chains(N_chains, N_atoms_in_chain)
 
-
-# Loading the system in the MDAnalysis universe:
+# Loading the system into MDAnalysis universe:
 traj = md.Universe(struct, trajectory)
 save_top = md.Universe(struct)
 print("Information about trajectory", trajectory)
 frames = len(traj.trajectory) # Determine number of frames in the trajectory
 timesteps = len(range(0, frames, stride)) - 1 # Determine number of frames to be used in analysis, including only every stride-th frame
 
-# Loading the crystal structure in the MDAnalysis universe:
+# Loading the crystal structure into MDAnalysis universe:
 crystal = md.Universe(CRYSTAL_STRUCTURE)
 crystal_box = crystal.trajectory[0].dimensions # dimensions of the sell. Needed for treating periodic boundaries.
-
 
 # MPI initialization:
 comm = MPI.COMM_WORLD
@@ -94,7 +84,6 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 # We divide computations between ranks across the time, so each process will analyze its own part of the trajectory
-
 local_size = timesteps // size # make sure timesteps equally divided between ranks (!!!)
 ind_start = rank * local_size
 traj_start = ind_start * stride
@@ -128,15 +117,17 @@ local_offsets = np.zeros((local_size, N_chains, n_waters, 3), dtype=np.float)
 local_laws_errors = np.zeros((local_size, N_chains, n_waters), dtype=np.float)
 
 
-## Initial pipeline of the LAWS method
+## Pipeline of the LAWS method
 
-## Selecting only heavy atoms in each chain
+# 1. Selecting only heavy atoms in each chain
 heavy_atoms = crystal.select_atoms('protein and not type H')
 crystal_heavy_atoms_chains = [
     crystal.select_atoms('protein and not type H ' + chain) for chain in chains
 ]
 
-## Detect coordinating heavy atoms from the crystal structure
+## Detect CWS around each protein chain in the crystal structure
+# Numbering of CWS must be consequtive (chain by chain) and should go after protein coordinates.
+# Naming of CWS oxygens must be resname HOH, atom name OW.
 crystal_waters_chains = [
     crystal.select_atoms('resname HOH and name OW and index {}:{}'.format(
         N_atoms_in_chain * N_chains + 3 * n_waters * chain,
@@ -144,10 +135,7 @@ crystal_waters_chains = [
     ) for chain in range(N_chains) 
 ]
 
-# Needing for visualization
-connectors_set = create_connectors(crystal_waters_chains, crystal_heavy_atoms_chains, crystal_box)
-
-# Find coordinating heavy atoms for each CWS in a crystal 
+# Find coordinating heavy atoms for each CWS around each chain 
 crystal_waters_info = [
         [
             find_N_closest_heavy_atoms(water.position, heavy_atoms, crystal_box) for water in crystal_waters_chains[chain]
@@ -155,6 +143,13 @@ crystal_waters_info = [
     for chain in range(N_chains)
 ]
 
+# Find distances from each protein atom to CWS
+heavy_distances = [
+    [d for _, _, d in crystal_waters_info[chain]]
+    for chain in range(N_chains)
+]
+
+# Create MDAnalysis selector for each heavy atom coordinating the CWS
 selectors = [
     [
         [
@@ -165,11 +160,6 @@ selectors = [
     for chain in range(N_chains)         
 ]
 
-# exctract distances to each atom
-heavy_distances = [
-    [d for _, _, d in crystal_waters_info[chain]]
-    for chain in range(N_chains)
-]
 
 
 all_waters = traj.select_atoms('name OW') 
@@ -178,6 +168,9 @@ all_chains_protein = traj.select_atoms('protein')
 # memory allocation
 crystal_water_positions = np.empty((n_waters, 3))
 crystal_water_errors = np.empty((n_waters))
+
+# Need or visualization
+connectors_set = create_connectors(crystal_waters_chains, crystal_heavy_atoms_chains, crystal_box)
 
 for j in range(N_chains):
     
